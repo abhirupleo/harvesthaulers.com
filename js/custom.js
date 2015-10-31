d80b5b480b42116394f16b3f66e4425a5da3323e
Abhirup Panja <AbhirupLeo@Leomessi.connectify>
2015-11-01 05:10:30 +0530

    extended the site to other features

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$('.fa-bars').on('click',function(){
-		$('.mininav2').css("display","block");
+		$('.mininav2').css("transform","translateX(0%)");
 	});
 	$('.close').on('click',function(){
-		$('.mininav2').css("transform","none");
+		$('.mininav2').css("transform","translateX(-100%)");
 	});
 });
